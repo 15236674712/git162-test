934880c38ba5ab8264e16b779d59fb178065de6c
zhangcn <15236674712@163.com>
2019-09-25 12:07:14 +0800

    开发者2 添加好几行代码

--- src/test/java/com/baizhi/TestGit.java
+++ src/test/java/com/baizhi/TestGit.java
@@ -5,6 +5,10 @@ public class TestGit {
     public void testa() {
         System.out.println("==创建一个方法==");
         System.out.println("==打印了一行数据==");
+        int a=100;
+        int b=200;
+        int c=a+b;
+        System.out.println("==开发者2添加=="+c);
     }
 
 }
